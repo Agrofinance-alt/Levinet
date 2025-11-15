e7c793bdcdfadd1b77fcffc62437e85401dd922f
LEVINET <leslylevinet@gmail.com>
2025-11-16 00:33:30 +0100

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,53 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import BottomNavigation from "@/components/BottomNavigation";
+import Home from "@/pages/Home";
+import Add from "@/pages/Add";
+import AddCredit from "@/pages/AddCredit";
+import AddCompteCourant from "@/pages/AddCompteCourant";
+import AddCartePointage from "@/pages/AddCartePointage";
+import Profile from "@/pages/Profile";
+import Credit from "@/pages/Credit";
+import Epargne from "@/pages/Epargne";
+import Solde from "@/pages/Solde";
+import Contencieux from "@/pages/Contencieux";
+import Performance from "@/pages/Performance";
+import Corbeille from "@/pages/Corbeille";
+import ClientDetails from "@/pages/ClientDetails";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/add" component={Add} />
+      <Route path="/add/credit" component={AddCredit} />
+      <Route path="/add/compte-courant" component={AddCompteCourant} />
+      <Route path="/add/carte-pointage" component={AddCartePointage} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/credit" component={Credit} />
+      <Route path="/epargne" component={Epargne} />
+      <Route path="/solde" component={Solde} />
+      <Route path="/contencieux" component={Contencieux} />
+      <Route path="/performance" component={Performance} />
+      <Route path="/corbeille" component={Corbeille} />
+      <Route path="/client/:id" component={ClientDetails} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Router />
+        <BottomNavigation />
+        <Toaster />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
